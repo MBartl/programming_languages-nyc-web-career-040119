8bc17b3b38d93e2326455f17eb22b60066897096
Matthew Bartl <mattbartl@gmail.com>
2019-03-22 01:08:29 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,13 +3,13 @@ def reformat_languages(languages)
   languages.each do |style, data1|
     data1.each do |language, data2|
       data2.each do |type_category, type|
-        new_hash[language] = {:type => type, :style => [style]}
+        new_hash[language] = {:type => type, :style => []}
       end
     end
   end
   if new_hash[:javascript][:style].length == 1
     javascript_style_change = new_hash[:javascript][:style].to_s
-    languages[:oo] do |style, data1|
+    languages.each do |style, data1|
       javascript_style_change.push(style)
       return javascript_style_change
     end
