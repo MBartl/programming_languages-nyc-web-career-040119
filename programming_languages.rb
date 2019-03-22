1e644fd722edb9f7f0ef664f437b6fe69056caca
Matthew Bartl <mattbartl@gmail.com>
2019-03-22 00:35:22 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |type, data|
     data.each do |language, type|
-      new_hash[language] = language
+      new_hash[language] = {}
     end
   end
   return new_hash
