9b2936feffba1a59e6a062d58489c5030554c316
Matthew Bartl <mattbartl@gmail.com>
2019-03-22 00:44:55 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,6 @@ def reformat_languages(languages)
   languages.each do |style, data1|
     data1.each do |language, data2|
       data2.each do |type_category, type|
-        type_holder = 
         new_hash[language] = {:type => type, :style => [style]}
       end
     end
