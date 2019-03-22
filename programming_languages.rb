4eb8022f0182729bae326af07639dc3a391ecfec
Matthew Bartl <mattbartl@gmail.com>
2019-03-22 01:20:15 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -7,13 +7,9 @@ def reformat_languages(languages)
       end
     end
   end
-  new_hash.each do |language_new, data3|
-    return new_hash
-    languages.each do |style, data1|
-      data1.each do |language, data2|
-        if new_hash[language_new].to_s == languages[style][language].to_s
-          return check
-          new_hash[language][:style] << languages[style].to_s
+  languages.each do |style, data1|
+    data1.each do |language, data2|
+      new_hash[language][:style] << languages[style].to_s
         end
       end
     end
