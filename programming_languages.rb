6b189b66753c2666b1c47bd86003a260bc0d5786
Matthew Bartl <mattbartl@gmail.com>
2019-03-22 00:55:55 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -9,7 +9,7 @@ def reformat_languages(languages)
   end
   if new_hash[:javascript][:style] == [:functional]
     javascript_array_change = new_hash[:javascript][:style]
-    return javascript_array_change.inspect
+    return javascript_array_change.class
   end
 end
 
